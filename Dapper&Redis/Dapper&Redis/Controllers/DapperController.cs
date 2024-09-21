c9287ca48466c582de1df6b3e1f4759ca9db5b10
Khaled Ibrahim <kamel.mohamed@intalio.com>
2024-09-21 22:37:24 +0300

    Impelemtn Redis

--- Dapper&Redis/Dapper&Redis/Controllers/DapperController.cs
+++ Dapper&Redis/Dapper&Redis/Controllers/DapperController.cs
@@ -1,6 +1,9 @@
 using Dapper_Redis.Entity;
 using Dapper_Redis.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+using System.Text;
 
 namespace Dapper_Redis.Controllers
 {
@@ -9,24 +12,49 @@ namespace Dapper_Redis.Controllers
     public class DapperController : ControllerBase
     {
         private readonly ICompanyRepository companyRepository;
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private readonly IDistributedCache _cache;
 
         private readonly ILogger<DapperController> _logger;
 
-        public DapperController(ILogger<DapperController> logger, ICompanyRepository companyRepository)
+        public DapperController(ILogger<DapperController> logger, ICompanyRepository companyRepository, IDistributedCache cache)
         {
             _logger = logger;
             this.companyRepository = companyRepository;
+            _cache = cache;
         }
 
         [HttpGet]
         public async Task<List<Company>> GetAllCompnies()
         {
-            var companies = await companyRepository.GetCompanies();
-            return companies.ToList();
+            var cachKey = "All_Companies";
+            var companyList=new List<Company>();
+            var cachedData = await _cache.GetAsync(cachKey);
+            if (cachedData != null)
+            {
+                // If data found in cache, encode and deserialize cached data
+                var cachedDataString = Encoding.UTF8.GetString(cachedData);
+                companyList = JsonSerializer.Deserialize<List<Company>>(cachedDataString) ?? new List<Company>();
+
+            }
+            else
+            {
+                // If not found, then fetch data from database
+                var companies = await companyRepository.GetCompanies();
+                companyList=companies.ToList(); 
+                // serialize data
+                var cachedDataString = JsonSerializer.Serialize(companies);
+                var newDataToCache = Encoding.UTF8.GetBytes(cachedDataString);
+
+                // set cache options 
+                var options = new DistributedCacheEntryOptions()
+                    .SetAbsoluteExpiration(DateTime.Now.AddMinutes(2))
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(1));
+                // Add data in cache
+
+                await _cache.SetAsync(cachKey, newDataToCache, options);
+               
+            }
+            return companyList.ToList();
         }
         [HttpGet("{id}", Name = "CompanyById")]
         public async Task<IActionResult> GetCompany(int id)
--- Dapper&Redis/Dapper&Redis/Program.cs
+++ Dapper&Redis/Dapper&Redis/Program.cs
@@ -7,6 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
 builder.Services.AddControllers();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration["RedisCacheOptions:Configuration"];
+    options.InstanceName = builder.Configuration["RedisCacheOptions:InstanceName"];
+});
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
